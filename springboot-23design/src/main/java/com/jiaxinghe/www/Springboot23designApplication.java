8a2cd4c3f4f54827d78e68e2bb4fb34ea9718e14
hejiaxing10808 <hejiaxing10808@hellobike.com>
2020-11-20 15:57:02 +0800

    添加23种设计模式

---
+++ springboot-23design/src/main/java/com/jiaxinghe/www/Springboot23designApplication.java
@@ -0,0 +1,13 @@
+package com.jiaxinghe.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot23designApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot23designApplication.class, args);
+	}
+
+}
